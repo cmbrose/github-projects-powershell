c895796b663a00c5afa33c2491f273697d50a7fe
Caleb Brose <5447118+cmbrose@users.noreply.github.com>
2023-06-21 18:59:14 -0500

    TEMP log exception message

--- types/project-v2.psm1
+++ types/project-v2.psm1
@@ -581,6 +581,7 @@ function Get-ProjectItemsByIdBatch {
         | ForEach-Object { $_.path[0] } # Index 0 is the node name
         | ForEach-Object { $nodeNameToIdMap[$_] }
 
+        Write-Warning $exception.Message
         Write-Warning "Could not load node ids $($badNodes -join ", ")"
 
         $ids = $ids | Where-Object { $badNodes -notcontains $_ }
